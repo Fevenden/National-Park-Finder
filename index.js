e5ab639c6763093b463bb1d1e8a687b14ce17a32
fte96 <ftevenden1@gmail.com>
2019-12-20 00:47:20 -0900

    everything is working as it should

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const myAPI_Key= 'RzDxuHnBlpeqCFfWUYmyPfUwwJr6iHkbaPfaNSkR'
 function handleSearchClick() {
   $('button').click( function(e) {
     e.preventDefault();
-    $('.js-results').removeClass('hidden');
     const state = document.getElementById('search').value.replace(/\s/g, "");
     const max = document.getElementById('resultNum').value.replace(/\s/g, "");
     getData(state, max)
@@ -13,9 +12,23 @@ function handleSearchClick() {
 }
 
 function getData(state, max) {
-  fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=${max}&api_key=${myAPI_Key}`)
-  .then(response => response.json())
-  .then(responseJson => displayData(responseJson))
+  if (state === "") {
+    $('.js-err').text('You have to enter a state.')
+  }else {
+    $('.js-err').empty();
+    fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${state}&limit=${max}&api_key=${myAPI_Key}`)
+    .then(response => response.json())
+    .then(responseJson => {
+      if (responseJson.total === "0") {
+        $('.js-err').text('make sure you enter the correct initials of the state you are searching')
+      }else {
+        $('.js-results').removeClass('hidden');
+        $('.js-err').empty();
+        displayData(responseJson)
+      }
+    })
+    .catch(err => $('.js-err').text(`Something went wrong: ${err.message}`))
+  }
 }
 
 function displayData(json) {
